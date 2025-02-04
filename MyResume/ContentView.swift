44a1fb804dc75fdcb068aa9e96cc0ed7346ecce2
Hailey <haileykuo96@gmail.com>
2025-02-04 10:30:46 +0800

    Initial Commit

---
+++ MyResume/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyResume
+//
+//  Created by 郭蕙瑄 on 2025/2/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyResume/MyResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyResumeApp.swift
+//  MyResume
+//
+//  Created by 郭蕙瑄 on 2025/2/4.
+//
+
+import SwiftUI
+
+@main
+struct MyResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
